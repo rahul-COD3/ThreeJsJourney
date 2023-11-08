fdae244d64b3b9f6c98240ae4db18cf8084bf2e5
Rahul Kumar <r1303yadav@gmail.com>
2023-11-08 15:13:53 +0530

    Animation

--- src/script.js
+++ src/script.js
@@ -2,18 +2,46 @@ import "./style.css";
 
 import * as THREE from "three";
 
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 1.5;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube3);
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 // Sizes
 const sizes = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
 // Camera
@@ -23,7 +51,7 @@ scene.add(camera);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("canvas.webgl"),
+  canvas: canvas,
 });
 
 renderer.setSize(sizes.width, sizes.height);
